671dc68e2e25c1127ede00eedf7da2cd50bce9cb
moyanmoyu <mx19921228@163.com>
2019-09-01 21:26:42 +0800

    删除table

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,17 @@ import Router from 'vue-router'
 //路由懒加载
 const Main = resolve => require(['../components/Main.vue'], resolve)
 const Index = resolve => require(['../components/index/index.vue'], resolve)
-const SimpleLine = resolve => require(['../components/D3/SimpleLine.vue'], resolve)
-const SimplePie = resolve => require(['../components/D3/SimplePie.vue'], resolve)
+const SimpleLine = resolve =>
+  require(['../components/D3/SimpleLine.vue'], resolve)
+const SimplePie = resolve =>
+  require(['../components/D3/SimplePie.vue'], resolve)
 const ChinaMap = resolve => require(['../components/D3/ChinaMap.vue'], resolve)
-const SimpleForce = resolve => require(['../components/D3/SimpleForce.vue'], resolve)
-const SimpleBar = resolve => require(['../components/D3/SimpleBar.vue'], resolve)
-const SimplePoints = resolve => require(['../components/D3/SimplePoints.vue'], resolve)
-const Form = resolve => require(['../components/Common/form.vue'], resolve)
-const Table = resolve => require(['../components/Common/table.vue'], resolve)
+const SimpleForce = resolve =>
+  require(['../components/D3/SimpleForce.vue'], resolve)
+const SimpleBar = resolve =>
+  require(['../components/D3/SimpleBar.vue'], resolve)
+const SimplePoints = resolve =>
+  require(['../components/D3/SimplePoints.vue'], resolve)
 
 const Bar = resolve => require(['../components/Echarts/bar.vue'], resolve)
 const Line = resolve => require(['../components/Echarts/line.vue'], resolve)
@@ -26,64 +29,63 @@ export default new Router({
     {
       path: '/',
       component: Main,
-      children: [{
-        path: '/',
-        name: 'index',
-        component: Index
-      },{
-        path: '/form',
-        name: 'form',
-        component: Form
-      },{
-        path: '/table',
-        name: 'table',
-        component: Table,
-        children: [{
-          path: 'add',
-          name: 'add',
-          component: Form
-        }]
-      },{
-        path: '/simpleline',
-        name: 'simpleline',
-        component: SimpleLine
-      },{
-        path: '/chinamap',
-        name: 'chinamap',
-        component: ChinaMap
-      },{
-        path: '/simplebar',
-        name: 'simplebar',
-        component: SimpleBar
-      },{
-        path: '/simpleforce',
-        name: 'simpleforce',
-        component: SimpleForce
-      },{
-        path: '/simplepoints',
-        name: 'simplepoints',
-        component: SimplePoints
-      },{
-        path: '/simplepie',
-        name: 'simplepie',
-        component: SimplePie
-      },{
-        path: '/echartspie',
-        name: 'echartspie',
-        component: Pie
-      },{
-        path: '/echartsline',
-        name: 'echartsline',
-        component: Line
-      },{
-        path: '/echartsbar',
-        name: 'echartsbar',
-        component: Bar
-      },{
-        path: '/echartsmap',
-        name: 'echartsmap',
-        component: Map
-      }]
+      children: [
+        {
+          path: '/',
+          name: 'index',
+          component: Index
+        },
+        {
+          path: '/simpleline',
+          name: 'simpleline',
+          component: SimpleLine
+        },
+        {
+          path: '/chinamap',
+          name: 'chinamap',
+          component: ChinaMap
+        },
+        {
+          path: '/simplebar',
+          name: 'simplebar',
+          component: SimpleBar
+        },
+        {
+          path: '/simpleforce',
+          name: 'simpleforce',
+          component: SimpleForce
+        },
+        {
+          path: '/simplepoints',
+          name: 'simplepoints',
+          component: SimplePoints
+        },
+        {
+          path: '/simplepie',
+          name: 'simplepie',
+          component: SimplePie
+        },
+        {
+          path: '/echartspie',
+          name: 'echartspie',
+          component: Pie
+        },
+        {
+          path: '/echartsline',
+          name: 'echartsline',
+          component: Line
+        },
+        {
+          path: '/echartsbar',
+          name: 'echartsbar',
+          component: Bar
+        },
+        {
+          path: '/echartsmap',
+          name: 'echartsmap',
+          component: Map
+        }
+      ]
     }
   ]
 })
